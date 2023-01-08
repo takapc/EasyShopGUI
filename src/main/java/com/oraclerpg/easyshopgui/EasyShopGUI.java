78da78e7c6863ba58ccdbefb2d8632efa7840ff6
takap_c <takapc0324@gmail.com>
2023-01-08 22:06:23 +0900

    [Add] First Commit

---
+++ src/main/java/com/oraclerpg/easyshopgui/EasyShopGUI.java
@@ -0,0 +1,17 @@
+package com.oraclerpg.easyshopgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyShopGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
